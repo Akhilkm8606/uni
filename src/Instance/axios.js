2a485dba2c0ddb73b981b477af89d52a7bb454ef
Akhil krishnan m <akhilkrishnan3860645@gmail.com>
2024-09-04 22:09:02 +0530

    s

--- src/Instance/axios.js
+++ src/Instance/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL : "http://localhost:5000/"
+    baseURL : "https://unified-cart-server.vercel.app/"
 })
 
 export default instance;
