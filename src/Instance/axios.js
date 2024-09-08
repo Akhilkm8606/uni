bbac059f2333212509ea40f6105f9fae4ff98636
Akhil krishnan m <akhilkrishnan3860645@gmail.com>
2024-09-08 23:15:01 +0530

    s

--- src/Instance/axios.js
+++ src/Instance/axios.js
@@ -1,8 +1,8 @@
-import axios from "axios";
-
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL : "https://unified-cart-server.vercel.app"
-})
+  baseURL: 'https://unified-cart-server.vercel.app',
+  withCredentials: true // Ensure credentials are sent
+});
 
 export default instance;
