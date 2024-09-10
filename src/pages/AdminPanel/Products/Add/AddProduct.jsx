3b4c25dab9c66fd2e0733f4a64b3f42e15927347
Akhil krishnan m <akhilkrishnan3860645@gmail.com>
2024-09-10 19:18:15 +0530

    s

--- src/pages/AdminPanel/Products/Add/AddProduct.jsx
+++ src/pages/AdminPanel/Products/Add/AddProduct.jsx
@@ -57,7 +57,9 @@ function AddProduct() {
 
       // Append each image file to the FormData object
       Array.from(images).forEach(images => formDataToSend.append('images', images));
-
+      console.log('Form Data:', formData);
+      console.log('Images:', images);
+      
       const response = await instance.post(`/api/v1/product/${user._id}`, formDataToSend, {
         withCredentials: true,
         headers: { "Content-Type": "multipart/form-data" }
