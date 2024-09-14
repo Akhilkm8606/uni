5b38130eef5a176359f806dd4a6745de84d9db6a
Akhil krishnan m <akhilkrishnan3860645@gmail.com>
2024-09-14 20:49:07 +0530

    s

--- src/components/adminDashBoard/Header.jsx
+++ src/components/adminDashBoard/Header.jsx
@@ -7,46 +7,34 @@ import { AiOutlineMail } from "react-icons/ai";
 import { IoMdHome, IoMdNotifications } from 'react-icons/io';
 import { userLogOut } from '../Redux/Slice/user';
 import { useDispatch, useSelector } from 'react-redux';
-import Theme from '../UserDashBoard/Layout/Theme';
+import { useNavigate } from 'react-router-dom';
 
 function Header({ handleOptionClick }) {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
   
+  const [profileState, setProfileState] = useState({
+    card: false,
+    msg: false,
+    nty: false,
+  });
 
-  const handleLogOut =()=>{
+  const handleLogOut = async () => {
     try {
-     
-      dispatch(userLogOut())
-      console.log('logoted');
+      await dispatch(userLogOut()); // Ensure this dispatch function returns a promise
+      console.log('Logged out');
+      navigate('/login'); // Redirect to login page
     } catch (error) {
       console.error("Error logging out:", error);
-
     }
-  }
- 
-
-  const [showCard, setShowCard] = useState(false);
-  const [showMsg, setShowMsg] = useState(false);
-  const [showNty, setShowNty] = useState(false);
+  };
 
   const handleProfile = (section) => {
-    switch (section) {
-      case 'card':
-        setShowCard(!showCard);
-        break;
-      case 'msg':
-        setShowMsg(!showMsg);
-        break;
-      case 'nty':
-        setShowNty(!showNty);
-        break;
-      default:
-        setShowCard(false);
-        setShowMsg(false);
-        setShowNty(false);
-        break;
-    }
+    setProfileState((prevState) => ({
+      ...prevState,
+      [section]: !prevState[section],
+    }));
   };
 
   return (
@@ -56,25 +44,25 @@ function Header({ handleOptionClick }) {
         <span><FaShopify /></span>
       </div>
       <div className='header-input'>
-        <input type="search" name="Search" id="Search" placeholder='Search' />
+        <input type="search" name="Search" id="Search" placeholder='Search' aria-label="Search" />
         <IoSearchOutline className='searchIcon' />
       </div>
       <div className='header-right'>
-      <Link to={"/"} ><IoMdHome className='icon-Home' /></Link>
+        <Link to={"/"}><IoMdHome className='icon-Home' /></Link>
 
         <AiOutlineMail onClick={() => handleProfile('msg')} className='icon-user' />
-        {showMsg && <div className='masseges'>Message Section</div>}
+        {profileState.msg && <div className='messages'>Message Section</div>}
 
         <IoMdNotifications onClick={() => handleProfile('nty')} className='icon-user' />
-        {showNty && <div className='notification'>Notification Section</div>}
+        {profileState.nty && <div className='notification'>Notification Section</div>}
 
         <FaCircleUser onClick={() => handleProfile('card')} className='icon-user' />
-        {showCard && (
+        {profileState.card && (
           <div className='profile_setting'>
             <h4>User Options</h4>
-            <Link className='profileLinks'  onClick={() => handleOptionClick("profile")}><FaUser /> Profile</Link>
-            <Link className='profileLinks' to={"/"} ><IoSettingsOutline /> Settings</Link>
-            <Link className='profileLinks' to={""} onClick={handleLogOut} > <IoExit /> Logout</Link>
+            <Link className='profileLinks' onClick={() => handleOptionClick("/MyAccount")}><FaUser /> Profile</Link>
+            <Link className='profileLinks' to="/settings"><IoSettingsOutline /> Settings</Link>
+            <Link className='profileLinks' to="#" onClick={handleLogOut}><IoExit /> Logout</Link>
           </div>
         )}
       </div>
