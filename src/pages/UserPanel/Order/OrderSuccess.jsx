f3d9bd94043f3ee4e203a7d713aed533bb90389e
Akhil krishnan m <akhilkrishnan3860645@gmail.com>
2024-10-14 22:16:31 +0530

    s

--- src/pages/UserPanel/Order/OrderSuccess.jsx
+++ src/pages/UserPanel/Order/OrderSuccess.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './order.css'; // Import the CSS file
 
 function OrderSuccess() {
   return (
@@ -7,13 +8,7 @@ function OrderSuccess() {
       <h1>Order Successful!</h1>
       <p>Your order has been placed successfully.</p>
       <p>Thank you for shopping with us!</p>
-      <div className="order-details">
-        <h2>Order Details:</h2>
-        <ul>
-          <li>Order Number: <strong>#12345</strong></li>
-          <li>Estimated Delivery: <strong>5-7 business days</strong></li>
-        </ul>
-      </div>
+     
       <div className="actions">
         <Link to="/" className="btn">Return to Home</Link>
         <Link to="/orders" className="btn">View Orders</Link>
